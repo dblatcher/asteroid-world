a3baf85880a2ee8cb5bd93c2f6e9313346c62202
David Blatcher <david.blatcher@graduate-jobs.com>
2021-02-27 21:27:04 +0000

    add to fake module exports

--- src/_fake-module.ts
+++ src/_fake-module.ts
@@ -1,4 +1,4 @@
-import { Thing, Force, ThingData, Shape, Geometry, RenderFunctions, World, CollisionDetection } from '../../worlds/src/index'
+import { Thing, Force, ThingData, Shape, Geometry, RenderFunctions, World, CollisionDetection, WorldConfig } from '../../worlds/src/index'
 
 
-export {Thing, Force, ThingData, Shape, Geometry, RenderFunctions, World, CollisionDetection}
+export {Thing, Force, ThingData, Shape, Geometry, RenderFunctions, World, CollisionDetection, WorldConfig}
