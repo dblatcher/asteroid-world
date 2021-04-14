8cef5369bf394faab3033ee16d09e3557534ff2d
David Blatcher <david.blatcher@graduate-jobs.com>
2021-04-14 10:48:45 +0100

    run script after window has loaded

--- src/asteroids/index.ts
+++ src/asteroids/index.ts
@@ -1,15 +1,19 @@
 import { AsteroidsGame } from './AsteroidsGame'
 import { gameWorld, levels } from './worldSetup'
 
-const game = new AsteroidsGame(
-    gameWorld, 
-    levels, 
-    50, 
-    document.querySelector('#gameCanvas'),
-    {
-        main: document.getElementById('gameContainer'),
-        score: document.getElementById('score'),
-        lives: document.getElementById('lives'),
-        level: document.getElementById('level'),
-    });
-(window as any).game = game;
+function init() {
+    const game = new AsteroidsGame(
+        gameWorld,
+        levels,
+        50,
+        document.querySelector('#gameCanvas'),
+        {
+            main: document.getElementById('gameContainer'),
+            score: document.getElementById('score'),
+            lives: document.getElementById('lives'),
+            level: document.getElementById('level'),
+        });
+    (window as any).game = game;
+}
+
+window.addEventListener('load', init, { once: true });
